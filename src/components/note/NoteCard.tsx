02aea88b97becc1eb5fdf9a8409c328892d68c39
ammarrqureshi <mamqureshi786@gmail.com>
2024-01-02 22:38:40 +0500

    build/added edit option to note card

--- src/components/note/NoteCard.tsx
+++ src/components/note/NoteCard.tsx
@@ -1,4 +1,5 @@
-import { Card, Text } from "@radix-ui/themes";
+import { Pencil1Icon } from "@radix-ui/react-icons";
+import { Card, IconButton, Text, Tooltip } from "@radix-ui/themes";
 
 interface NoteProps {
   title: string;
@@ -6,7 +7,7 @@ interface NoteProps {
 }
 export const NoteCard = ({ title, note }: NoteProps) => {
   return (
-    <Card asChild style={{ maxWidth: 350 }}>
+    <Card className="group" asChild style={{ maxWidth: 350 }}>
       <a href="#">
         <div className="rounded-md max-h-10 truncate max-w-px">
           <Text as="div" size="2" weight="bold">
@@ -15,6 +16,13 @@ export const NoteCard = ({ title, note }: NoteProps) => {
           <Text as="div" color="gray" size="2">
             {note}
           </Text>
+        </div>
+        <div className="absolute flex right-2 top-2 hidden group-hover:flex">
+          <Tooltip content="Edit">
+            <IconButton variant="outline">
+              <Pencil1Icon width="18" height="18" />
+            </IconButton>
+          </Tooltip>
         </div>
       </a>
     </Card>
