6167fa7b931d28249015be430695f36970470096
ammarrqureshi <mamqureshi786@gmail.com>
2024-01-08 19:14:35 +0500

    feat: all notebook page updated

--- src/pages/Notebooks.tsx
+++ src/pages/Notebooks.tsx
@@ -1,8 +1,19 @@
+import { NotebookCard } from "@/components/notebook/NotebookCard"
+import { notebooks } from "@/data"
+import { Flex, Heading } from "@radix-ui/themes"
 
 export const Notebooks = () => {
   return (
-      <div>
-        
+    <div>
+      <Heading>
+        All Notebooks </Heading>
+      <Flex gap="2" wrap="wrap">
+        {
+          notebooks.map(notebook => (
+            <NotebookCard {...notebook}></NotebookCard>
+          ))
+        }
+      </Flex>
     </div>
   )
 }
