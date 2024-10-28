e3d513517cec0fdea28f46a86fc2ca1dba9f4f4a
ammarrqureshi <mamqureshi786@gmail.com>
2024-10-28 14:15:16 +0500

    eslint issue fixed in input.tsx

--- src/components/ui/input.tsx
+++ src/components/ui/input.tsx
@@ -2,9 +2,11 @@ import * as React from "react";
 
 import { cn } from "@/lib/utils";
 
+// eslint-disable-next-line
 export interface InputProps
   extends React.InputHTMLAttributes<HTMLInputElement> {}
 
+// eslint-disable-next-line
 const Input = React.forwardRef<HTMLInputElement, InputProps>(
   ({ className, type, ...props }, ref) => {
     return (
