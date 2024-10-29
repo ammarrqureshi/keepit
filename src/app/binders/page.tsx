479399a2a5842a93a1616dc34927771d40172a55
ammarrqureshi <mamqureshi786@gmail.com>
2024-10-29 19:49:03 +0500

    binders cards added

--- src/app/binders/page.tsx
+++ src/app/binders/page.tsx
@@ -1,5 +1,22 @@
+import { Card } from "@/components/ui/card";
 import React from "react";
 
 export default function Binders() {
-  return <div>Binders</div>;
+  return (
+    <div className="flex gap-3">
+      <Card className="p-4">
+        <h1>My Diary</h1>
+        hello my self
+      </Card>
+      <Card className="p-4">
+        <h1>My Diary</h1>
+      </Card>
+      <Card className="p-4">
+        <h1>My Diary</h1>
+      </Card>
+      <Card className="p-4">
+        <h1>My Diary</h1>
+      </Card>
+    </div>
+  );
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@ import { Input } from "@/components/ui/input";
 export default function Home() {
   return (
     <div>
-      <div className="p-4 ">
+      <div>
         <Input placeholder="Start writing..." />
       </div>
     </div>
