e99586965a9446e2b21db6289f33a0ec9318e459
ammarrqureshi <mamqureshi786@gmail.com>
2024-10-30 16:38:25 +0500

    pages section on dash

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,22 @@
 import BinderWrapper from "@/components/binder-wrapper";
 import { Input } from "@/components/ui/input";
+import { Separator } from "@/components/ui/separator";
 export default function Home() {
   return (
     <div>
       <div>
         <Input placeholder="Start writing..." />
       </div>
+      <div className="flex flex-col gap-4 my-4">
+        <h2 className="font-bold text-2xl">Binders</h2>
+        <Separator />
+      </div>
       <div>
         <BinderWrapper />
+      </div>
+      <div className="flex flex-col gap-4 my-4">
+        <h2 className="font-bold text-2xl">Pages</h2>
+        <Separator />
       </div>
     </div>
   );
