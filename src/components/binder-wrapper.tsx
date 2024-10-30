460b821e47ad1e45c96d3bb41455728632e2b219
ammarrqureshi <mamqureshi786@gmail.com>
2024-10-30 17:50:58 +0500

    dummy binders data

--- src/components/binder-wrapper.tsx
+++ src/components/binder-wrapper.tsx
@@ -1,12 +1,42 @@
-import { Card, CardContent, CardHeader } from "@/components/ui/card";
+import {
+  Card,
+  CardContent,
+  CardFooter,
+  CardHeader,
+} from "@/components/ui/card";
 import {
   Carousel,
   CarouselContent,
   CarouselItem,
 } from "@/components/ui/carousel";
 import { Button } from "./ui/button";
-import { FiEdit2, FiPenTool } from "react-icons/fi";
+import { FiEdit2, FiPenTool, FiShare } from "react-icons/fi";
 
+const binders = [
+  {
+    title: "My diary",
+    description: "Lorepdf ajdf f dfdlj df ",
+    color: "cyan",
+  },
+  {
+    title: "My diary",
+    description:
+      "dfk dflkj dlfj dfkjdskfj dfj dkfj dfdjfj difdj fdkf jdf j dfdfj ddf ",
+    color: "zinc",
+  },
+  {
+    title: "My diary",
+    description:
+      "Lorepdf ajdf f dfdlj df Lorepdf ajdf f dfdlj df Lorepdf ajdf f dfdlj df Lorepdf ajdf f dfdlj df ",
+    color: "amber",
+  },
+  {
+    title: "My diary",
+    description:
+      "Lorepdf ajdf f dfdlj df Lorepdf ajdf f dfdlj df Lorepdf ajdf f dfdlj df Lorepdf ajdf f dfdlj df Lorepdf ajdf f dfdlj df Lorepdf ajdf f dfdlj df Lorepdf ajdf f dfdlj df Lorepdf ajdf f dfdlj df Lorepdf ajdf f dfdlj df Lorepdf ajdf f dfdlj df Lorepdf ajdf f dfdlj df Lorepdf ajdf f dfdlj df Lorepdf ajdf f dfdlj df Lorepdf ajdf f dfdlj df Lorepdf ajdf f dfdlj df Lorepdf ajdf f dfdlj df Lorepdf ajdf f dfdlj df Lorepdf ajdf f dfdlj df Lorepdf ajdf f dfdlj df Lorepdf ajdf f dfdlj df ",
+    color: "red",
+  },
+];
 export default function BinderWrapper() {
   return (
     <Carousel
@@ -16,28 +46,38 @@ export default function BinderWrapper() {
       className="w-full "
     >
       <CarouselContent>
-        {Array.from({ length: 5 }).map((_, index) => (
-          <CarouselItem key={index} className=" lg:basis-1/4">
+        {binders.map((binder, index) => (
+          <CarouselItem key={index} className="md:basis-1/4 lg:basis-1/4">
             <div className="p-1 e">
               <Card>
                 <CardHeader className="relative">
-                  <Button
-                    size="icon"
-                    variant="ghost"
-                    className="absolute  top-0 right-0"
-                  >
-                    <FiEdit2 />
-                  </Button>
-                  <div>
-                    <div className="flex gap-2  items-center ">
-                      <Button size="icon" variant="destructive">
-                        <FiPenTool />
-                      </Button>
-                      My Diary
-                    </div>
+                  <div className="flex gap-2  items-center ">
+                    <Button
+                      variant="secondary"
+                      size="icon"
+                      className={`bg-${binder.color}-500 text-background`}
+                    >
+                      <FiPenTool />
+                    </Button>
+                    {binder.title}
                   </div>
                 </CardHeader>
-                <CardContent>hre is na lorem ipsum noe etc et</CardContent>
+                <CardContent>
+                  <p className="text-xs text-zinc-700">{binder.description}</p>
+                </CardContent>
+                <CardFooter>
+                  <div className="flex justify-between items-center w-full">
+                    <p className="text-xs text-zinc-500">20 Feb 2024</p>
+                    <div className="flex justify-between gap-2">
+                      <Button size="smIcon" variant="outline">
+                        <FiShare />
+                      </Button>
+                      <Button size="smIcon" variant="outline">
+                        <FiEdit2 />
+                      </Button>
+                    </div>
+                  </div>
+                </CardFooter>
               </Card>
             </div>
           </CarouselItem>
