d3c1b800319fcf9e5fe8a527849bad6a3a5746ca
ammarrqureshi <mamqureshi786@gmail.com>
2024-11-08 17:51:23 +0500

    icon size fixed in binderbtncomponent

--- src/app/binders/[binderId]/page.tsx
+++ src/app/binders/[binderId]/page.tsx
@@ -1,6 +1,9 @@
 import { Heading1 } from "@/components/ui/heading";
 import { binders } from "@/data";
 import { Separator } from "@/components/ui/separator";
+import BinderIconButton from "@/components/binder-icon-button";
+import { FiPenTool } from "react-icons/fi";
+import { Button } from "@/components/ui/button";
 
 export default async function Binder({
   params,
@@ -17,6 +20,11 @@ export default async function Binder({
   return (
     <div>
       <div className="flex flex-col justify-between gap-4 py-4">
+        <BinderIconButton
+          className="p-8"
+          icon={FiPenTool}
+          iconStyle="scale-150"
+        />
         <Heading1>{binder.title}</Heading1>
         <Separator />
       </div>
--- src/components/binder-icon-button.tsx
+++ src/components/binder-icon-button.tsx
@@ -4,17 +4,19 @@ import type { IconType } from "react-icons";
 
 interface IconButtonProps extends React.ComponentProps<typeof Button> {
   icon: IconType;
+  iconStyle?: string;
   children: React.ReactNode;
 }
 
 export default function BinderIconButton({
   icon: Icon,
+  iconStyle,
   children,
   ...props
 }: IconButtonProps) {
   return (
     <Button size="icon" {...props}>
-      <Icon />
+      <Icon className={iconStyle} />
       {children}
     </Button>
   );
